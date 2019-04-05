c0ff47ba581cb9faa889a9cd240e29252071b447
Renata Pereira <renata.pereirans@gmail.com>
2019-04-05 14:42:16 -0300

    Refactoring

--- features/step_definitions/validar_texto_steps.rb
+++ features/step_definitions/validar_texto_steps.rb
@@ -1,13 +1,12 @@
-Dado("que esteja no site vcmais.com") do 
-	visit 'https://vcmais.com/'
+Dado('que esteja no site vcmais.com') do 
+  visit 'https://vcmais.com/'
 end
 
-Quando("eu digitar meu e-mail no campo e-mail e clicar em cadastre-se") do
-	fill_in('email',:with=>'renata.pereira@vcmais.com')
-	click_button('btn-signup')	
+Quando('eu digitar meu e-mail no campo e-mail e clicar em cadastre-se') do
+  fill_in('email', with: 'renata.pereira@vcmais.com')
+  click_button('btn-signup')
 end
 
-Entao("Verei o loading e a mensagem {string}") do |mensagem_sucesso|
-	assert_text('Em breve, para você.')
-	# expect(mensagem).to eq('Em breve, para você.')
+Entao('Verei o loading e a mensagem {string}') do |mensagem_sucesso|
+  expect(mensagem_sucesso).to eq('Em breve, para você.')
 end
